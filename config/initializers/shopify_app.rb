9f40931b555e92af45fecbca15aa7b5c73751e85
Tripti <tripti.bhangade@gemsessence.com>
2019-12-06 15:32:57 +0530

    shopify app implementation

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -2,3 +2,19 @@ shop_url = "https://#{ENV["SHOPIFY_API_KEY"]}:#{ENV["SHOPIFY_PASSWORD"]}@#{ENV["
 ShopifyAPI::Base.site = shop_url
 ShopifyAPI::Base.api_version = ENV["SHOPIFY_API_VERSION"]
 ShopifyAPI::Session.setup(api_key: ENV['shopify_api_key'], secret: ENV['shopify_secret_key'])
+
+ShopifyApp.configure do |config|
+  config.application_name = "my-demo-store-partners-1234"
+  config.api_key = ENV['shopify_api_key']
+  config.secret = ENV['shopify_secret_key']
+  config.old_secret = ""
+  config.scope = "read_products" # Consult this page for more scope options:
+                                 # https://help.shopify.com/en/api/getting-started/authentication/oauth/scopes
+  config.embedded_app = true
+  config.after_authenticate_job = false
+  config.api_version = "2019-10"
+  config.session_repository = Shop
+end
+
+# ShopifyApp::Utils.fetch_known_api_versions                        # Uncomment to fetch known api versions from shopify servers on boot
+# ShopifyAPI::ApiVersion.version_lookup_mode = :raise_on_unknown    # Uncomment to raise an error if attempting to use an api version that was not previously known
--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2019_12_05_121215) do
+
+  create_table "shops", force: :cascade do |t|
+    t.string "shopify_domain", null: false
+    t.string "shopify_token", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["shopify_domain"], name: "index_shops_on_shopify_domain", unique: true
+  end
 
 end
